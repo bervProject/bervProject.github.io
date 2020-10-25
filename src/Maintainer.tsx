51540039db7287ae4f54a8023f5dfdf7e8bbb273
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-10-26 00:41:20 +0700

    update react script & maintainer

--- src/Maintainer.tsx
+++ src/Maintainer.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import Card from '@material-ui/core/Card';
-import { CardContent, Typography, Grid } from '@material-ui/core';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import Gravatar from "react-gravatar";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import {
+  Avatar,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       padding: theme.spacing(3, 2),
-    }
-  }),
+    },
+    large: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+  })
 );
 const Maintainer: React.FC = () => {
   const classes = useStyles();
@@ -17,20 +29,27 @@ const Maintainer: React.FC = () => {
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <Card>
+            <CardHeader
+              avatar={
+                <Avatar className={classes.large}>
+                  <Gravatar
+                    email="bervianto.leo@gmail.com"
+                    className={classes.large}
+                  />
+                </Avatar>
+              }
+              title="Bervianto Leo Pratama"
+              subheader="bervianto.leo@gmail.com"
+            ></CardHeader>
             <CardContent>
-              <Typography variant="h2">
-                Bervianto Leo Pratama
-      </Typography>
-              <Typography variant="body1">
-                bervianto.leo@gmail.com
-      </Typography>
+              <Typography variant="body1">Social Media</Typography>
+              <a href="https://github.com/berviantoleo"><GitHubIcon/></a>
             </CardContent>
           </Card>
-
         </Grid>
       </Grid>
     </div>
   );
-}
+};
 
 export default Maintainer;
